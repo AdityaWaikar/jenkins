93f0a99dae23f4cbd4be34e66f9b1f29dd198e05
AdityaWaikar <101462042+AdityaWaikar@users.noreply.github.com>
2025-05-08 17:13:26 +0530

    Update newShared.groovy
    using 2 function for testing ,

--- vars/newShared.groovy
+++ vars/newShared.groovy
@@ -1,4 +1,9 @@
 def firstcall() {
-sh "echo  coditas "
-sh date
+ echo "Hello from shared library!"
+    sh 'echo Running on $(hostname)'
+}
+
+// Method 2: Named method (must be called with the method name)
+def withName(String name) {
+    echo "Hello, ${name}!"
 }
